f0d6721dd581be54f933a5e2c68c0186020b4c24
Jake W <jake82w@gmail.com>
2022-03-19 10:59:30 -0400

    Fix transaction sample
    1. Main is calling Sample method which does not exist. Updated to use correct TxnSample
    2. Converted to use more readable string interop instead of string format
    3. Catch uses the when filter to reduce code
    4. Other minor optimizations and code readability improvements

--- transaction.cs
+++ transaction.cs
@@ -5,119 +5,119 @@ using Npgsql;
 
 namespace Cockroach
 {
-  class MainClass
-  {
-    static void Main(string[] args)
+    class MainClass
     {
-      var connStringBuilder = new NpgsqlConnectionStringBuilder();
-      connStringBuilder.Host = "localhost";
-      connStringBuilder.Port = 26257;
-      connStringBuilder.SslMode = SslMode.Require;
-      connStringBuilder.Username = "{username}";
-      connStringBuilder.Password = "{password}";
-      connStringBuilder.Database = "bank";
-      connStringBuilder.TrustServerCertificate = true;
-      Simple(connStringBuilder.ConnectionString);
-    }
-
-    static void TransferFunds(NpgsqlConnection conn, NpgsqlTransaction tran, int from, int to, int amount)
-    {
-      int balance = 0;
-      using (var cmd = new NpgsqlCommand(String.Format("SELECT balance FROM accounts WHERE id = {0}", from), conn, tran))
-      using (var reader = cmd.ExecuteReader())
-      {
-        if (reader.Read())
+        static void Main(string[] args)
         {
-          balance = reader.GetInt32(0);
-        }
-        else
-        {
-          throw new DataException(String.Format("Account id={0} not found", from));
-        }
-      }
-      if (balance < amount)
-      {
-        throw new DataException(String.Format("Insufficient balance in account id={0}", from));
-      }
-      using (var cmd = new NpgsqlCommand(String.Format("UPDATE accounts SET balance = balance - {0} where id = {1}", amount, from), conn, tran))
-      {
-        cmd.ExecuteNonQuery();
-      }
-      using (var cmd = new NpgsqlCommand(String.Format("UPDATE accounts SET balance = balance + {0} where id = {1}", amount, to), conn, tran))
-      {
-        cmd.ExecuteNonQuery();
-      }
-    }
-
-    static void TxnSample(string connString)
-    {
-      using (var conn = new NpgsqlConnection(connString))
-      {
-        conn.Open();
-
-        // Create the "accounts" table.
-        new NpgsqlCommand("CREATE TABLE IF NOT EXISTS accounts (id INT PRIMARY KEY, balance INT)", conn).ExecuteNonQuery();
-
-        // Insert two rows into the "accounts" table.
-        using (var cmd = new NpgsqlCommand())
-        {
-          cmd.Connection = conn;
-          cmd.CommandText = "UPSERT INTO accounts(id, balance) VALUES(@id1, @val1), (@id2, @val2)";
-          cmd.Parameters.AddWithValue("id1", 1);
-          cmd.Parameters.AddWithValue("val1", 1000);
-          cmd.Parameters.AddWithValue("id2", 2);
-          cmd.Parameters.AddWithValue("val2", 250);
-          cmd.ExecuteNonQuery();
+            var connStringBuilder = new NpgsqlConnectionStringBuilder();
+            connStringBuilder.Host = "localhost";
+            connStringBuilder.Port = 26257;
+            connStringBuilder.SslMode = SslMode.Require;
+            connStringBuilder.Username = "{username}";
+            connStringBuilder.Password = "{password}";
+            connStringBuilder.Database = "bank";
+            connStringBuilder.TrustServerCertificate = true;
+            TxnSample(connStringBuilder.ConnectionString);
         }
 
-        // Print out the balances.
-        System.Console.WriteLine("Initial balances:");
-        using (var cmd = new NpgsqlCommand("SELECT id, balance FROM accounts", conn))
-        using (var reader = cmd.ExecuteReader())
-        while (reader.Read())
-          Console.Write("\taccount {0}: {1}\n", reader.GetValue(0), reader.GetValue(1));
-
-        try
+        static void TransferFunds(NpgsqlConnection conn, NpgsqlTransaction tran, int from, int to, int amount)
         {
-          using (var tran = conn.BeginTransaction())
-          {
-            tran.Save("cockroach_restart");
-            while (true)
+            using (var cmd = new NpgsqlCommand($"SELECT balance FROM accounts WHERE id = {from}", conn, tran))
+            using (var reader = cmd.ExecuteReader())
             {
-              try
-              {
-                TransferFunds(conn, tran, 1, 2, 100);
-                tran.Commit();
-                break;
-              }
-              catch (NpgsqlException e)
-              {
-                // Check if the error code indicates a SERIALIZATION_FAILURE.
-                if (e.ErrorCode == 40001)
+                if (!reader.Read())
                 {
-                  // Signal the database that we will attempt a retry.
-                  tran.Rollback("cockroach_restart");
+                    throw new DataException($"Account id={from} not found");
                 }
-                else
+
+                int balance = reader.GetInt32(0);
+                if (balance < amount)
                 {
-                  throw;
+                    throw new DataException($"Insufficient balance in account id={from}");
                 }
-              }
             }
-          }
-        }
-        catch (DataException e)
-        {
-          Console.WriteLine(e.Message);
+
+            using (var cmd = new NpgsqlCommand($"UPDATE accounts SET balance = balance - {amount} where id = {from}", conn, tran))
+            {
+                cmd.ExecuteNonQuery();
+            }
+
+            using (var cmd = new NpgsqlCommand($"UPDATE accounts SET balance = balance + {amount} where id = {to}", conn, tran))
+            {
+                cmd.ExecuteNonQuery();
+            }
         }
 
-        // Now printout the results.
-        Console.WriteLine("Final balances:");
-        using (var cmd = new NpgsqlCommand("SELECT id, balance FROM accounts", conn))
-        using (var reader = cmd.ExecuteReader())
-        while (reader.Read())
-          Console.Write("\taccount {0}: {1}\n", reader.GetValue(0), reader.GetValue(1));
-      }
+        static void TxnSample(string connString)
+        {
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+                // Create the "accounts" table.
+                using (var cmd = new NpgsqlCommand("CREATE TABLE IF NOT EXISTS accounts (id INT PRIMARY KEY, balance INT)", conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                // Insert two rows into the "accounts" table.
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "UPSERT INTO accounts(id, balance) VALUES(@id1, @val1), (@id2, @val2)";
+                    cmd.Parameters.AddWithValue("id1", 1);
+                    cmd.Parameters.AddWithValue("val1", 1000);
+                    cmd.Parameters.AddWithValue("id2", 2);
+                    cmd.Parameters.AddWithValue("val2", 250);
+                    cmd.ExecuteNonQuery();
+                }
+
+                // Print out the balances.
+                System.Console.WriteLine("Initial balances:");
+                using (var cmd = new NpgsqlCommand("SELECT id, balance FROM accounts", conn))
+                using (var reader = cmd.ExecuteReader())
+                    while (reader.Read())
+                        Console.Write($"\taccount {reader.GetValue(0)}: {reader.GetValue(1)}{Environment.NewLine}");
+
+                try
+                {
+                    using (var tran = conn.BeginTransaction())
+                    {
+                        tran.Save("cockroach_restart");
+                        while (true)
+                        {
+                            try
+                            {
+                                TransferFunds(
+                                    conn: conn,
+                                    tran: tran,
+                                    from: 1,
+                                    to: 2,
+                                    amount: 100);
+
+                                tran.Commit();
+                                break;
+                            }
+                            catch (NpgsqlException e) when (e.ErrorCode == 40001) // indicates a SERIALIZATION_FAILURE
+                            {
+                                // Signal the database that we will attempt a retry.
+                                tran.Rollback("cockroach_restart");
+                            }
+                        }
+                    }
+                }
+                catch (DataException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                // Now printout the results.
+                Console.WriteLine("Final balances:");
+                using (var cmd = new NpgsqlCommand("SELECT id, balance FROM accounts", conn))
+                using (var reader = cmd.ExecuteReader())
+                    while (reader.Read())
+                        Console.Write($"\taccount {reader.GetValue(0)}: {reader.GetValue(1)}{Environment.NewLine}");
+            }
+        }
     }
-  }
 }
